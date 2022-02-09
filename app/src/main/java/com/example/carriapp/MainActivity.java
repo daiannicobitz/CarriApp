fbc0eb35849124b9a0ada1168d4d71052aee6429
Fachurraxx <ramagrippo@gmail.com>
2022-02-09 15:45:13 -0300

    anduvo insertar dato en BD obtenido de pantalla

--- app/src/main/java/com/example/carriapp/MainActivity.java
+++ app/src/main/java/com/example/carriapp/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.carriapp;
 import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.carriapp.Config.Constantes;
 import com.example.carriapp.DataBase.AppDataBase;
 import com.example.carriapp.Entidades.Carribar;
 
@@ -15,19 +20,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_agregar_carribar);
 
-        db = Room.databaseBuilder(getApplicationContext(),AppDataBase.class,"produccion")
+        db = Room.databaseBuilder(getApplicationContext(),AppDataBase.class, Constantes.BD_NAME)
                 .allowMainThreadQueries()
                 .build();
 
-        Carribar carriPrueba = new Carribar("prueba2","AA 7013","20","23",
-                "3434474355", true, true, false, false, false,
-                false, true);
+        Button botonAgregar = (Button) findViewById(R.id.button);
 
-        db.carribarDao().insert(carriPrueba);
+        botonAgregar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText editTextNombreCarri = (EditText) findViewById(R.id.editTextNombre);
+                String nombreCarribar = editTextNombreCarri.getText().toString();
 
-        System.out.println("ACA LLEGÓ");
+                Carribar carriPrueba = new Carribar(nombreCarribar ,"AA 7013","20","23",
+                        "3434474355", true, true, false, false, false,
+                        false, true);
+
+                db.carribarDao().insert(carriPrueba);
+
+                System.out.println("ACA LLEGÓ");
+            }
+        });
+
+
 
         System.out.println(db.carribarDao().count());
 
