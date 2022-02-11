0a68dd8940ecea0c1614c58aba6867ceff2db73c
Fachurraxx <ramagrippo@gmail.com>
2022-02-11 17:42:03 -0300

    MUestra de Datos en la pantalla

--- app/src/main/java/com/example/carriapp/MainActivity.java
+++ app/src/main/java/com/example/carriapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.carriapp;
 import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.carriapp.Config.Constantes;
@@ -14,6 +19,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     AppDataBase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +39,46 @@ public class MainActivity extends AppCompatActivity {
 //
 //        System.out.println(db.carribarDao().count());
 
+
+
+        inicializarComponentes();
+
+    }
+
+    public void inicializarComponentes(){
+
         List<Carribar> listaCarribares = db.carribarDao().getAllCarribares();
-        Carribar carriPrueba2 = listaCarribares.get(0);
-        textViewNombreCarribar.setText(carriPrueba2.getNombre());
+        Carribar carribarAMostrar = listaCarribares.get(0);
 
+        ((TextView) findViewById(R.id.textViewNombre)).setText(carribarAMostrar.getNombre());
+        ((TextView) findViewById(R.id.textViewDireccion)).setText(carribarAMostrar.getDireccion());
+        ((TextView) findViewById(R.id.textViewDistancia)).setText("Calcular Distancia");
 
+        //Calcular hora si esta abierto o cerrado y cambiar visibilidad
+        ((TextView) findViewById(R.id.textViewHoraCierre)).setText(carribarAMostrar.getHoraCierre());
+        ((TextView) findViewById(R.id.textViewHoraApertura)).setText(carribarAMostrar.getHoraApertura());
 
+        ((TextView) findViewById(R.id.textViewContacto)).setText(carribarAMostrar.getContacto());
+
+        if(!carribarAMostrar.getHayHamburguesa())
+            ((TextView) findViewById(R.id.textViewHamburguesas)).setVisibility(View.GONE);
+
+        if(!carribarAMostrar.getHayBondiola())
+            ((TextView) findViewById(R.id.textViewBondiola)).setVisibility(View.GONE);
+
+        if(!carribarAMostrar.getHayChoripan())
+            ((TextView) findViewById(R.id.textViewChoripanes)).setVisibility(View.GONE);
+
+        if(!carribarAMostrar.getHayMilanesa())
+            ((TextView) findViewById(R.id.textViewMilanesa)).setVisibility(View.GONE);
+
+        if(!carribarAMostrar.getHayPancho())
+            ((TextView) findViewById(R.id.textViewPanchos)).setVisibility(View.GONE);
+
+        if(!carribarAMostrar.getHayPapasFritas())
+            ((TextView) findViewById(R.id.textViewPapasFritas)).setVisibility(View.GONE);
+
+        if(!carribarAMostrar.getHayPizza())
+            ((TextView) findViewById(R.id.textViewPizza)).setVisibility(View.GONE);
     }
 }
