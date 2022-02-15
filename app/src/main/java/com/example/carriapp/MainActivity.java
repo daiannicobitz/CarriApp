2a8ef4af220c7a267ce9311688795f4b9dcacb0c
Fachurraxx <ramagrippo@gmail.com>
2022-02-15 15:24:38 -0300

    Manejo entre diferentes actividades

--- app/src/main/java/com/example/carriapp/MainActivity.java
+++ app/src/main/java/com/example/carriapp/MainActivity.java
@@ -1,17 +1,39 @@
 package com.example.carriapp;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botonAgregar;
+    Button botonVer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        botonAgregar = (Button) findViewById(R.id.buttonAgregar);
+        botonAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), AgregarCarribarActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
 
+        botonVer = (Button) findViewById(R.id.button2Ver);
+        botonVer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), VerCarribarActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 
 }
