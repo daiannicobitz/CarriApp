9a8d7bba7a13077bc84a49912f5cc2ba83f14689
Fachurraxx <ramagrippo@gmail.com>
2022-02-15 15:04:52 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/carriapp/MainActivity.java
+++ app/src/main/java/com/example/carriapp/MainActivity.java
@@ -1,20 +1,8 @@
 package com.example.carriapp;
 
-import android.arch.persistence.room.Room;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-
-import com.example.carriapp.Config.Constantes;
-import com.example.carriapp.DataBase.AppDataBase;
-import com.example.carriapp.Entidades.Carribar;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
