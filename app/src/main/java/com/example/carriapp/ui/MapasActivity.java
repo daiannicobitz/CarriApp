f25ad8809aa9adeca30cd68b5119d9f166ac6c6a
Fachurraxx <ramagrippo@gmail.com>
2022-02-21 12:34:11 -0300

    Ya Markers y direccion!

--- app/src/main/java/com/example/carriapp/ui/MapasActivity.java
+++ app/src/main/java/com/example/carriapp/ui/MapasActivity.java
@@ -1,20 +1,35 @@
 package com.example.carriapp.ui;
 
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.carriapp.FetchURL;
 import com.example.carriapp.R;
+import com.example.carriapp.TaskLoadedCallback;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapasActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapasActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
 
     private GoogleMap mMap;
+    private MarkerOptions inicio, fin;
+    private Polyline currentPolyline;
+    Button getDirection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +37,27 @@ public class MapasActivity extends AppCompatActivity implements OnMapReadyCallba
         setContentView(R.layout.activity_mapas);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
+        getDirection = findViewById(R.id.btnGetDirection);
+
+
+        inicio = new MarkerOptions().position(new LatLng(27.658143, 85.3199503)).title("Location 1");
+        fin = new MarkerOptions().position(new LatLng(27.667491, 85.3208583)).title("Location 2");
+                //.icon(BitmapDescriptorFactory.fromResource(R.drawable.choripan));
+
         SupportMapFragment mapFragment;
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
-
         mapFragment.getMapAsync(this);
+
+        getDirection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actualizarMapa();
+                new FetchURL(MapasActivity.this).execute(getUrl(inicio.getPosition(), fin.getPosition(), "driving"), "driving");
+            }
+        });
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     *
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -48,6 +68,67 @@ public class MapasActivity extends AppCompatActivity implements OnMapReadyCallba
                 .position(sydney)
                 .title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        Log.d("mylog", "Added Markers");
+        mMap.addMarker(inicio);
+        mMap.addMarker(fin);
     }
+
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Mode
+        String mode = "mode=" + directionMode;
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + getString(R.string.google_maps_key);
+        return url;
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyline != null)
+            currentPolyline.remove();
+        currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void ejecutarActualizacionMapa(){
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+    }
+
+    public void actualizarMapa(){
+        if(noTienePermiso()){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},9999);
+        } else {
+            ejecutarActualizacionMapa();
+        }
+    }
+
+    private boolean noTienePermiso(){
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED;
+    }
+    @Override
+    public void onRequestPermissionsResult (int requestCode , String [] permissions , int [] grantResults ){
+
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            ejecutarActualizacionMapa();
+        }else{
+            Toast.makeText(MapasActivity.this , "No tiene permiso para obtener la ubicacion actual", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
 }
 
